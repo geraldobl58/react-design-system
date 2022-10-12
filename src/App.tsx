cb894c0d4c6203f9cc41cc8105254c3405767ee0
Geraldo Luiz <geraldobl58@gmail.com>
2022-10-12 16:16:55 -0300

    init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './styles/global.css';
+
+export function App() { 
+  return (
+    <h1 className="bg-cyan-300 text-2xl">React</h1>
+  )
+}
+
---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Text } from ".";
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+} as Meta
+
+export const Default: StoryObj = {}
